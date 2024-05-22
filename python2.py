18a567d579d6e94650b3f2c676c064b7b667b8f8
dhaval-codes <nimisha25dhaval@gmail.com>
2024-05-23 02:44:32 +0530

    learned basic syntax

---
+++ python2.py
@@ -0,0 +1,15 @@
+if 5 < 2:
+    print("5 is greater than 2")
+
+x = 9.74
+y = "Topper's CGPA is"
+print(y,x)
+
+# this is a comment code
+
+# a = 20
+# b = 25
+# print(a+b)
+
+if x > 9:
+    print("Topper has score more than 9")
