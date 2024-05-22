822624d3a3a0860fe9f6d424b3155d9ec4eea8be
dhaval-codes <nimisha25dhaval@gmail.com>
2024-05-23 02:53:18 +0530

    understanding variables basics

---
+++ python3.py
@@ -0,0 +1,29 @@
+x = 4 # this is an int variable
+y = "Sally" # this is a string variable
+z = True # this is a boolean variable
+
+print(x == "4")
+
+# specify the data type of a variable, this can be done with casting.
+
+a = str(3) # this is "3"
+b = int(3) # this is 3
+c = float(3.2) # this is 3.2
+
+print(type(a),type(b),type(c))
+
+#double and single quotes means same
+
+name = "John"
+Name = "John"
+
+print(name == Name, "name terminal")
+
+# also variables are case sensitive
+
+marks = 98
+Marks = 84
+
+# Marks won't overwrite marks
+
+print(marks, Marks)
