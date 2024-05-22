3bcb76224be32462e9f60ab08710bd0ee4d3fb4a
dhaval-codes <nimisha25dhaval@gmail.com>
2024-05-23 03:03:41 +0530

    learned assinging multiple variable

---
+++ python5.py
@@ -0,0 +1,13 @@
+x,y,z = "Orange", "Banana", "Grapes"
+print(x)
+print(y)
+print(z)
+
+# new way of defining same value to different variable
+a = b = c = "Mango"
+print(a,b,c)
+
+#destructuring the list
+fruits = ["mango", "banana", "grapes", "apple"]
+p,q,r,s = fruits
+print(p,q,r,s)
