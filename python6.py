da3a61d94d7e8b9f64619f0bb6987e12ed1fe122
dhaval-codes <nimisha25dhaval@gmail.com>
2024-05-23 23:22:35 +0530

    learned various output techniques

---
+++ python6.py
@@ -0,0 +1,19 @@
+# python output variabls
+
+x = "python is awesome"
+print(x)
+
+a = "python "
+b = 'is '
+c = "awesome "
+
+print(a, b, c)
+
+print(a + b + c)
+
+q = 5
+y = 10
+print(q+y)
+
+#will give error
+print(x + q)
