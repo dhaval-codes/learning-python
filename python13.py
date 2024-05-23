29ad63efd88c81f14d22f79c2780a381021d9830
dhaval-codes <nimisha25dhaval@gmail.com>
2024-05-24 01:05:38 +0530

    Learned things about modifying strings

---
+++ python13.py
@@ -0,0 +1,21 @@
+# Modifying Strings
+
+# upper case
+a = "hello world"
+print(a.upper())
+
+#lower case
+b = "HEllo WOrLd"
+print(b.lower())
+
+#removes white spaces
+c = "Hello Word !  "
+print(c.strip())
+
+#replace string
+d = "Hello World!"
+print(d.replace("W","D"))
+
+#split string
+e = "Hello Mr Beast"
+print(e.split(" ")) # splits from the empty spaces
