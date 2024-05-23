a83cd3a2fc72db47f1d9c81d094289a689e7a412
dhaval-codes <nimisha25dhaval@gmail.com>
2024-05-24 00:56:36 +0530

    learned slicing strings in py in various ways

---
+++ python12.py
@@ -0,0 +1,17 @@
+#slicing strings
+
+b = "hello World"
+print(b[2:5])
+
+# slice from the start
+c = "dhaval j prasad"
+print(c[:6])
+
+# sclice from the end
+
+d = "dua lipa"
+print(d[4:])
+
+# indexing from right side of the string
+e = "Hello World!"
+print(e[-5:-2])
