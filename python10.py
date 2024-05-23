2f0175f7bfb733def7d99e0bf9f1ceafaad057fb
dhaval-codes <nimisha25dhaval@gmail.com>
2024-05-24 00:26:31 +0530

    leaned casting in python

---
+++ python10.py
@@ -0,0 +1,14 @@
+#python casting: when we want to specify a particular type on to a variable
+
+x = int(1) # x will be 1
+y = int(2.8) # y wiil be 2
+z = int("3") # z will be 3
+
+x = float(1) # x will be 1.0
+y = float(2.6) # y will be 2.6
+z = float("3") # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+x = str("s2") # x will be 's2'
+y = str(2) # y will be '2'
+z = str(3.0) # z will be '3.0'
